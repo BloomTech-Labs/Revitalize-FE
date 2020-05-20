29981c5774272fde453afb4818b83c13ae02d278
Chase-42 <chaseofthecollins@gmail.com>
2020-05-19 18:01:42 -0600

    Added some more questions

--- src/views/FAQ/FAQ.js
+++ src/views/FAQ/FAQ.js
@@ -12,22 +12,31 @@ function FAQ() {
 				<h2>Frequently Asked Questions</h2>
 				<Accordion
 					title="What is ReVitalize?"
-					content="<p>A modern approach to building your community, ReVitalize is a crowdfunding platform that creates apprenticeship opportunities to develop your skills to gain licensing.</p>"
+					content="<p>A modern approach to building your community, ReVitalize is a crowdfunding platform that creates apprenticeship 
+					opportunities to develop your skills to gain licensing.</p>"
 				/>
 				<Accordion
 					title="How do I get involved?"
 					content="<p>First you are going to want to register with ReVitalize. 
-					Once you've registered you have two options, one is to browse projects and find one you want to join. 
-					Once you have been accepted to a project you are now an Apprentice. The other option is to start your own project, which will make you a Project Manager.</p>"
+					If you are somone looking to develop their trade by being mentored and gaining experience on a real-life project all you need to do is find a project that excites you and apply.
+					If you or your business are interested in setting up community projects you can click on Create Project and fill out the form.
+					If you are an individual who is interested in mentoring apprentices go ahead and find a project you are interested in and apply, 
+					you will have a few more steps to take so that we can authenticate your licensing and know how many apprentices you are willing to take on.</P>"
 				/>
 				<Accordion
-					title="Can I use a custom domain?"
+					title="What is an Apprentice?"
 					content="
-   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-   </br>
-   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-   </br>
-   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>"
+   <p>An Apprentice is somone looking to develop their trade through being mentored by a Trades Professional to gain skills and experience towards their licensing.</p>"
+				/>
+				<Accordion
+					title="What is a Trades Professional?"
+					content="
+   <p>A Trades Professional is somone with a licensed specialization who is willing to donate time to mentor 1 or more Apprentices.</p>"
+				/>
+				<Accordion
+					title="What is a Project Manager?"
+					content="
+   <p>A Project Manager is an individual or business with an interest in setting up community projects.</p>"
 				/>
 			</div>
 			<Footer />
